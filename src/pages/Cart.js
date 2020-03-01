fffd9d4a83da2655457ac8559f247792164b6f93
Andrew Innocent <innocent.andrew@gmail.com>
2020-03-01 15:47:25 -0500

    Simplified total calculation logic

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,30 +1,27 @@
-import React, {useState, useContext, useEffect} from "react"
+import React, {useContext} from "react"
 import {Context} from "../components/AppContext"
 import CartItem from "../components/CartItem"
 
 
 function Cart() {
-    const PRICE = 5.99
-    const [total, setTotal] = useState(0)
-    const {cartItems} = useContext(Context)
-    const cartItemElements = cartItems.map(item => (
-      <CartItem
-        key={item.id}
-        item={item}/>
-    ))
-    
-    useEffect(() => setTotal(cartItems.length * PRICE), [cartItems])
-
-    return (
-        <main className="cart-page">
-            <h1>Check out</h1>
-            {cartItemElements}
-            <p className="total-cost">Total: {total.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
-            <div className="order-button">
-                <button>Place Order</button>
-            </div>
-        </main>
-    )
+  const {cartItems} = useContext(Context)
+  const total = 5.99 * cartItems.length
+  const cartItemElements = cartItems.map(item => (
+    <CartItem
+      key={item.id}
+      item={item}/>
+  ))
+  
+  return (
+      <main className="cart-page">
+          <h1>Check out</h1>
+          {cartItemElements}
+          <p className="total-cost">Total: {total.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
+          <div className="order-button">
+              <button>Place Order</button>
+          </div>
+      </main>
+  )
 }
 
 export default Cart
